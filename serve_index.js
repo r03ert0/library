a07041ad5252d65a386536776eec4f2191675b7e
r03ert0 <roberto.toro@gmail.com>
2021-11-01 10:46:30 +0100

    linting

--- serve_index.js
+++ serve_index.js
@@ -1,22 +1,22 @@
-var finalhandler = require('finalhandler')
-var http = require('http')
-var serveIndex = require('serve-index')
-var serveStatic = require('serve-static')
+var finalhandler = require('finalhandler');
+var http = require('http');
+var serveIndex = require('serve-index');
+var serveStatic = require('serve-static');
 
 // Serve directory indexes for public/ folder (with icons)
-var index = serveIndex('public/', {'icons': true})
+var index = serveIndex('public/', {'icons': true});
 
 // Serve up public/ folder files
-var serve = serveStatic('public/')
+var serve = serveStatic('public/');
 
 // Create server
-var server = http.createServer(function onRequest(req, res){
-  var done = finalhandler(req, res)
+var server = http.createServer(function onRequest(req, res) {
+  var done = finalhandler(req, res);
   serve(req, res, function onNext(err) {
-    if (err) return done(err)
-    index(req, res, done)
-  })
-})
+    if (err) { return done(err); }
+    index(req, res, done);
+  });
+});
 
 // Listen
-server.listen(3000)
+server.listen(3000);
