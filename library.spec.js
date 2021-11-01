f061e59e97f5421380b06a9965f2619cf142bbae
r03ert0 <roberto.toro@gmail.com>
2021-11-01 11:07:26 +0100

    library spec

---
+++ library.spec.js
@@ -0,0 +1,22 @@
+// const importBooks = (req, res)
+it('should throw an error if the path field is incorrect')
+it('should import books from a list if isbn is given')
+it('should import books from a list if title is given')
+it('should import books from a list if authors are given')
+
+// const queryAllBooks = async (req, res)
+it('should return a list with all books')
+
+// const servePDF = (req, res)
+it('should serve a book pdf if the path is correct')
+it('should throw an error if the book path is incorrect')
+
+// const removeBook = async (req, res);
+it('should correctly remove a book if information is correct')
+it('should throw an error if book information is incorrect')
+
+// const methodGet = (req, res, next);
+it('should correctly dispatch GET functions')
+
+// const methodPost = async (req, res, next);
+it('should correctly dispatch POST functions')
