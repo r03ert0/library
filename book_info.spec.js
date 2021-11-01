b8f18f5313c568c4a47ae09e8fdb92a45a245c6c
r03ert0 <roberto.toro@gmail.com>
2021-11-01 10:46:09 +0100

    start spec

---
+++ book_info.spec.js
@@ -0,0 +1,21 @@
+// const parseData = (data)
+it('should handle entries without thumbnail')
+
+// const getBookInfoFromDB = async (query)
+it('should appropriately unescape path');
+it('should throw an error for incorrect path')
+
+// const getBookInfoFromGoogle = async (query)
+
+// const addBookToDB = (book)
+
+// const getBookInfo = async (query)
+
+// const importBooks = async (list)
+
+// const queryAllBooks = async ()
+
+// const updateBook = (book)
+
+// const removeBook = (book)
+
